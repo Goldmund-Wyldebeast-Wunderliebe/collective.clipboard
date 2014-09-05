5be9faf6c162c9bd80efcbae34b8715d6b357bf7
Pawel Lewicki <lewicki@gw20e.com>
2014-09-05 09:54:03 +0200

    Added Dutch translations

---
+++ src/collective/clipboard/rebuild_i18n.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+I18NDUDE="../../../../../bin/i18ndude"
+I18NDOMAIN="collective.clipboard"
+
+# Synchronise the templates and scripts with the .pot.
+# All on one line normally:
+$I18NDUDE rebuild-pot --pot locales/${I18NDOMAIN}.pot \
+    --merge locales/collective.clipboard-additional.pot \
+    --create ${I18NDOMAIN} \
+   .
+
+# Synchronise the resulting .pot with all .po files
+for po in locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
+    $I18NDUDE sync --pot locales/${I18NDOMAIN}.pot $po
+done
+
