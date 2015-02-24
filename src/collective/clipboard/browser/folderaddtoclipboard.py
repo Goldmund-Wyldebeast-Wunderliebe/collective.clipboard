ee9e699225de36a423e731249caf5f47c81906a8
Pawel Lewicki <lewicki@gw20e.com>
2015-02-24 17:19:25 +0100

    Added folder_contents action to add items for the folder view to the clipboard

---
+++ src/collective/clipboard/browser/folderaddtoclipboard.py
@@ -0,0 +1,16 @@
+from Products.Five import BrowserView
+from plone import api
+from collective.clipboard.utils import add_to_clipboard
+
+
+class AddToClipboard(BrowserView):
+
+    def __call__(self):
+        request = self.request
+        paths = request.get('paths', None)
+        targets = [b.getObject() for b in api.portal.get_tool('portal_catalog')(path={'query':paths, 'depth':0})]
+        targets = filter(None, targets)
+        for target in targets:
+            add_to_clipboard(target.UID())
+        api.portal.show_message(message=(u'Item added to the clipboard'), request=request)
+        request.response.redirect(request.get('HTTP_REFERER'))
